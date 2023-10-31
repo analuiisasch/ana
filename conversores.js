9f1984ace70434c331303f3aec3d2baa72b2d7e4
ana luisa <140508741+analuiisasch@users.noreply.github.com>
2023-10-31 09:29:45 -0300

    Add files via upload

---
+++ conversores.js
@@ -0,0 +1,13 @@
+const numero = 456;
+const numeroString = "456a";
+
+//1
+console.log(numero == numeroString); 
+console.log (numero === numeroString);
+
+//2
+//console.log(numero + numeroString);
+
+//função Number() e a função (String)
+
+console.log(numero + Number(numeroString));
